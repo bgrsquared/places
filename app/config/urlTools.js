b6b437fd66b031777d58e939945e798d7e39770b
chroth7 <chroth@me.com>
2016-03-01 09:23:20 +0100

    Clean url and fix path.

--- app/config/urlTools.js
+++ app/config/urlTools.js
@@ -1,15 +1,22 @@
 export const setUrl = (router, country, filters) => {
   const { pre, suf, inf, lin, mod, reg } = filters;
+  const query = {
+    country,
+    v: '0.1', // url version
+    m: btoa(mod),
+  };
+
+  if (!mod) {
+    query.p = btoa(unescape(encodeURIComponent(pre.join(','))));
+    query.s = btoa(unescape(encodeURIComponent(suf.join(','))));
+    query.i = btoa(unescape(encodeURIComponent(inf.join(','))));
+    query.l = btoa(lin);
+  } else {
+    query.r = btoa(unescape(encodeURIComponent(reg.toString())));
+  }
+
   router.push({
-    query: {
-      country,
-      v: '0.1', // url version
-      p: btoa(unescape(encodeURIComponent(pre.join(',')))),
-      s: btoa(unescape(encodeURIComponent(suf.join(',')))),
-      i: btoa(unescape(encodeURIComponent(inf.join(',')))),
-      l: btoa(lin),
-      m: btoa(mod),
-      r: btoa(unescape(encodeURIComponent(reg.toString()))),
-    },
+    pathname: window.location.pathname,
+    query,
   });
 };
